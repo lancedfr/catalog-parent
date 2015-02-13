59467c59245f050274b6d4dacb5f10c43f6c7d97
ShaneV <shanevdbroek@gmail.com>
2015-02-13 09:17:40 +0200

    Test skeleton for Article

---
+++ catalog-repository/src/test/java/com/catalog/repository/dao/ArticleDaoImplIT.java
@@ -0,0 +1,11 @@
+package com.catalog.repository.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by ShaneMK on 2015/02/13.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ArticleDaoImplIT {
+}
